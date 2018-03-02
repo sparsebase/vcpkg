da6c40adba285c9b2aea3fc09e048b9e1b323161
Nan Zhang <zhangnan@baysenvision.com>
2018-03-02 16:10:57 +0800

    necessary change to use CUDA 8.0; platform toolset v140; and compile the opencv[cuda]

--- ports/cuda/portfile.cmake
+++ ports/cuda/portfile.cmake
@@ -20,7 +20,12 @@ if (NVCC)
         RESULT_VARIABLE error_code)
 endif()
 
-set(CUDA_REQUIRED_VERSION "V9.0.0")
+# Sample output
+# NVIDIA (R) Cuda compiler driver
+# Copyright (c) 2005-2016 NVIDIA Corporation
+# Built on Sat_Sep__3_19:05:48_CDT_2016
+# Cuda compilation tools, release 8.0, V8.0.44
+set(CUDA_REQUIRED_VERSION "V8.0.0")
 
 if (error_code)
     message(FATAL_ERROR "Could not find CUDA. Before continuing, please download and install CUDA  (${CUDA_REQUIRED_VERSION} or higher) from:"
@@ -28,18 +33,13 @@ if (error_code)
                         "\nAlso ensure vcpkg has been rebuilt with the latest version (v0.0.104 or later)")
 endif()
 
-# Sample output:
-# NVIDIA (R) Cuda compiler driver
-# Copyright (c) 2005-2016 NVIDIA Corporation
-# Built on Sat_Sep__3_19:05:48_CDT_2016
-# Cuda compilation tools, release 8.0, V8.0.44
 string(REGEX MATCH "V([0-9]+)\\.([0-9]+)\\.([0-9]+)" CUDA_VERSION ${NVCC_OUTPUT})
 message(STATUS "Found CUDA ${CUDA_VERSION}")
 set(CUDA_VERSION_MAJOR ${CMAKE_MATCH_1})
 #set(CUDA_VERSION_MINOR ${CMAKE_MATCH_2})
 #set(CUDA_VERSION_PATCH ${CMAKE_MATCH_3})
 
-if (CUDA_VERSION_MAJOR LESS 9)
+if (CUDA_VERSION_MAJOR LESS 8)
     message(FATAL_ERROR "CUDA ${CUDA_VERSION} but ${CUDA_REQUIRED_VERSION} is required. Please download and install a more recent version of CUDA from:"
                         "\n    https://developer.nvidia.com/cuda-downloads\n")
 endif()
